224cfe0a53bf78a51d440a659039b6f332e13d88
tysbox <tysbox@mx>
2026-04-13 00:27:44 +0900

    chore: prune repository to keep only frontend and dmp (remove other files)

--- system_backup_ver2/bin/fix_service.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo sed -i 's/^CPUSchedulingPolicy=rr/#CPUSchedulingPolicy=rr/' /etc/systemd/system/run_sox_fifo.service
-sudo sed -i 's/^CPUSchedulingPriority=48/#CPUSchedulingPriority=48/' /etc/systemd/system/run_sox_fifo.service
-sudo sed -i 's/^OOMScoreAdjust=-999/#OOMScoreAdjust=-999/' /etc/systemd/system/run_sox_fifo.service
-sudo systemctl daemon-reload
-sudo systemctl restart run_sox_fifo.service
-echo "Done!"
